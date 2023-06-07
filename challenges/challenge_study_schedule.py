ab144f119afbf573d0dd4c2e7eb7d0d682f29c07
nickolasvm <nickolas64vm@gmail.com>
2023-06-07 15:57:33 -0300

    feat: Implemented algorithm study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,15 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    """Returns the students that were still studying at given time"""
+    if target_time is None:
+        return None
+
+    counter = 0
+
+    for clock_in, clock_out in permanence_period:
+        if not isinstance(clock_in, int) or not isinstance(clock_out, int):
+            return None
+
+        if int(clock_in) <= target_time and int(clock_out) >= target_time:
+            counter += 1
+
+    return counter
