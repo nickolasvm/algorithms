eaba82a6c112f68bdca6360c6e4c1d630bacdfd2
nickolasvm <nickolas64vm@gmail.com>
2023-06-08 16:41:50 -0300

    feat: Implemented algorithm find_duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,16 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    nums_dict = {}
+
+    for n in nums:
+        if type(n) != int or n < 0:
+            return False
+
+        if nums_dict.get(n) is None:
+            nums_dict[n] = 1
+        else:
+            nums_dict[n] += 1
+
+    if len(nums) == len(nums_dict) or len(nums) <= 1:
+        return False
+
+    return max(nums_dict, key=nums_dict.get)
