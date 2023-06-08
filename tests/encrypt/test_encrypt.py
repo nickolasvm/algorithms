c7a53778daa88f3153234690a682abf67d49e2bf
nickolasvm <nickolas64vm@gmail.com>
2023-06-08 13:08:21 -0300

    feat: Implemented tests for the algorithm encrypt_message

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,25 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    # with success
+    message, key = "message", 3
+    assert encrypt_message(message, key) == "sem_egas"
+    message, key = "test_message", 4
+    assert encrypt_message(message, key) == "egassem__tset"
+    message, key = "my_message", 1
+    assert encrypt_message(message, key) == "m_egassem_y"
+
+    # with success when key is out of range
+    message, key = "message", 7
+    assert encrypt_message(message, key) == "egassem"
+
+    # failing type validation
+    message, key = 123, 3
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(message, key)
+
+    message, key = "message", "3"
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message(message, key)
